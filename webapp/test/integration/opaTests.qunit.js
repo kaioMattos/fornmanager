4a40b11001c0f34eaad5e43b976a0c46208a731f
Kaio Mattos <kaiomattos@MacBook-Air-de-Kaio.local>
2024-11-11 10:20:01 -0300

    push init project

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["gfex/petrobras/fornmanager/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"gfexpetrobras/fornmanager/test/unit/controller/main.controller"
+], function () {
+	"use strict";
+});
